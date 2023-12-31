d7ec8af446b3b245ee3bf506e7bb9e1b60b08bc4
John Pothier <john.pothier7@gmail.com>
2023-12-31 13:34:33 -0500

    fix

--- src/git.rs
+++ src/git.rs
@@ -6,7 +6,13 @@ use std::{
 
 #[cfg(debug_assertions)]
 fn run_command<'a>(program: &'a str, args: impl IntoIterator<Item = &'a str>) -> Result<String> {
-    let output = Command::new(program).args(args).output()?;
+    let args_vec: Vec<&str> = args.into_iter().collect();
+    print!("{}", program);
+    for arg in &args_vec {
+        print!(" {}", arg);
+    }
+
+    let output = Command::new(program).args(args_vec).output()?;
     let s = String::from_utf8(output.stdout).unwrap().trim().to_owned();
     println!("{}", s);
     Ok(s)
